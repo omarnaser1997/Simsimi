96bd48f17dc926f1c85c5740e4a85359c7b1f046
omarnaser1997 <omarnaser1997@gmail.com>
2018-04-09 15:09:40 -0700

    first commit

---
+++ app/src/main/java/com/examduple2sasd22/accoundomar/simsimi/Models/ChatModel.java
@@ -0,0 +1,32 @@
+package com.examduple2sasd22.accoundomar.simsimi.Models;
+
+/**
+ * Created by accoundOmar on 7/17/2017.
+ */
+
+public class ChatModel {
+    public String message;
+    public boolean isSend;
+
+    public ChatModel(String message, boolean isSend) {
+        this.message = message;
+        this.isSend = isSend;
+    }
+    public ChatModel(){}
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public void setSend(boolean send) {
+        isSend = send;
+    }
+}
